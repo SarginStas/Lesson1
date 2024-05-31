000449de171712c6f93c1e2a80166080c204dafb
AnnaRubina <anna_mihailova@bk.ru>
2024-05-31 11:37:01 +0300

    Добавлен тест по поиску пользователя с ID=5

--- src/test/java/lesson1/ARTests.java
+++ src/test/java/lesson1/ARTests.java
@@ -5,22 +5,23 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static config.Urls.USER5;
 import static config.Urls.USERS;
 import static io.restassured.RestAssured.given;
 
-public class SSTests {
+public class ARTests {
     ApplicationProperties properties = new ApplicationProperties();
 
-    @Test(description = "Получение списка пользователей.")
-    public void getUsers() {
-        String url = properties.readProperty("env.dev.url") + USERS;
+    @Test(description = "Поиск пользователя по ID=5")
+    public void getUsersID() {
+        String url = properties.readProperty("env.dev.url") + USER5;
         Response response = given()
                 .when()
                 .get(url)
                 .then()
                 .extract().response();
         Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertTrue(response.body().asString().contains("\"George\""),
-                "Пользователь не найден.");
+        Assert.assertTrue(response.body().asString().contains("\"Charles\""),
+                "ID=5 не найден");
     }
 }
