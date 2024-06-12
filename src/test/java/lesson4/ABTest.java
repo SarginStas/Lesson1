d8e175d255a27f681035588c78e3600d9b820174
BurnaevaAnna <burnaeva.annet@gmail.com>
2024-06-12 13:08:37 +0300

    udpate test manage Booking with RestHelpers

--- src/test/java/lesson4/ABTest.java
+++ src/test/java/lesson4/ABTest.java
@@ -1,5 +1,6 @@
 package lesson4;
 
+import api.RestHelper;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import models.json.response.Booking;
@@ -8,8 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import config.ApplicationProperties;
 
-import static config.Urls.BOOK;
-import static config.Urls.BOOK_ID;
+import static config.Urls.*;
 import static io.restassured.RestAssured.given;
 
 public class ABTest {
@@ -17,7 +17,7 @@ public class ABTest {
 
     @Test(description = "Manage booking")
     public void manageBooking() {
-        String url = properties.readProperty("env.dev.url2") + BOOK;
+        String baseUrl = properties.readProperty("env.dev.url2");
         String url2 = properties.readProperty("env.dev.url2") + BOOK_ID;
         Booking booking = Booking.builder()
                 .firstname("Jim")
@@ -27,22 +27,11 @@ public class ABTest {
                 .bookingdates(new BookingDates("2018-01-01", "2019-01-01"))
                 .additionalneeds("Breakfast")
                 .build();
-        Response postResponse = given()
-                .contentType(ContentType.JSON)
-                .body(booking)
-                .when()
-                .post(url)
-                .then()
-                .extract().response();
-
+        Response postResponse = RestHelper.postRequestWithBody(baseUrl, BOOK, booking);
 
         int bookingId = postResponse.jsonPath().getInt("bookingid");
-        Response getResponse = given()
-                .when()
-                .pathParam("id", bookingId)
-                .get(url2)
-                .then()
-                .extract().response();
+        String bookingEndpointWithId = "/booking/" + bookingId;
+        Response getResponse = RestHelper.getRequest(baseUrl, bookingEndpointWithId);
 
         Booking bookingAfterChange = Booking.builder()
                 .firstname("Jim")
@@ -52,31 +41,77 @@ public class ABTest {
                 .bookingdates(new BookingDates("2024-07-01", "2024-07-07"))
                 .additionalneeds("Dinner")
                 .build();
-        Response putResponse = given()
-                .auth()
-                .preemptive()
-                .basic("admin", "password123")
-                .contentType(ContentType.JSON)
-                .body(bookingAfterChange)
-                .when()
-                .pathParam("id", bookingId)
-                .put(url2)
-                .then()
-                .extract().response();
-
-        Response updateGetResponse = given()
-                .when()
-                .pathParam("id", bookingId)
-                .get(url2)
-                .then()
-                .extract().response();
+        String token = RestHelper.getToken("admin", "password123", baseUrl + AUTH);
+        Response putResponse = RestHelper.putRequestWithBodyAndToken(baseUrl, bookingEndpointWithId, bookingAfterChange, token);
         Assert.assertEquals(postResponse.getStatusCode(), 200);
         Assert.assertEquals("Breakfast", booking.getAdditionalneeds());
         Assert.assertEquals(getResponse.getStatusCode(), 200);
         Assert.assertEquals(putResponse.getStatusCode(), 200);
         Assert.assertEquals("Dinner", bookingAfterChange.getAdditionalneeds());
-        Assert.assertEquals(updateGetResponse.getStatusCode(), 200);
-
     }
+
+//    @Test(description = "Manage booking")
+//    public void manageBooking2() {
+//        String url = properties.readProperty("env.dev.url2") + BOOK;
+//        String url2 = properties.readProperty("env.dev.url2") + BOOK_ID;
+//        Booking booking = Booking.builder()
+//                .firstname("Jim")
+//                .lastname("Brown")
+//                .totalprice(111)
+//                .depositpaid(true)
+//                .bookingdates(new BookingDates("2018-01-01", "2019-01-01"))
+//                .additionalneeds("Breakfast")
+//                .build();
+//        Response postResponse = given()
+//                .contentType(ContentType.JSON)
+//                .body(booking)
+//                .when()
+//                .post(url)
+//                .then()
+//                .extract().response();
+//
+//
+//        int bookingId = postResponse.jsonPath().getInt("bookingid");
+//        Response getResponse = given()
+//                .when()
+//                .pathParam("id", bookingId)
+//                .get(url2)
+//                .then()
+//                .extract().response();
+//
+//        Booking bookingAfterChange = Booking.builder()
+//                .firstname("Jim")
+//                .lastname("Brown")
+//                .totalprice(130)
+//                .depositpaid(true)
+//                .bookingdates(new BookingDates("2024-07-01", "2024-07-07"))
+//                .additionalneeds("Dinner")
+//                .build();
+//        Response putResponse = given()
+//                .auth()
+//                .preemptive()
+//                .basic("admin", "password123")
+//                .contentType(ContentType.JSON)
+//                .body(bookingAfterChange)
+//                .when()
+//                .pathParam("id", bookingId)
+//                .put(url2)
+//                .then()
+//                .extract().response();
+//
+//        Response updateGetResponse = given()
+//                .when()
+//                .pathParam("id", bookingId)
+//                .get(url2)
+//                .then()
+//                .extract().response();
+//        Assert.assertEquals(postResponse.getStatusCode(), 200);
+//        Assert.assertEquals("Breakfast", booking.getAdditionalneeds());
+//        Assert.assertEquals(getResponse.getStatusCode(), 200);
+//        Assert.assertEquals(putResponse.getStatusCode(), 200);
+//        Assert.assertEquals("Dinner", bookingAfterChange.getAdditionalneeds());
+//        Assert.assertEquals(updateGetResponse.getStatusCode(), 200);
+//
+//    }
 
 }
